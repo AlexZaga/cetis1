56ebe8195944bc2894a6f25a984db18c05760032
Fermin <fpalacios7@hotmail.com>
2023-08-17 11:54:11 -0600

    parte1

--- julian/CONTROL/App.js
+++ julian/CONTROL/App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react'
+import Main from './src/views/Main'
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>CONTROL</Text>
-      <img src = CNA.PNG alt = "text">
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Main />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#28f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ julian/CONTROL/src/views/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Home from './Home'
+import About from './About'
+
+const Stack =createNativeStackNavigator()
+
+export default function Main(){
+return(
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen name='About' component={About}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+)
+}
