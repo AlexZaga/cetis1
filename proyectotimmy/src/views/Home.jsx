915a3031ea830015da33f607d8a15a55b1a015ef
Jesus lc <loringcivil@gmail.com>
2023-08-17 12:16:42 -0600

    Proyecto final Timmy v1

---
+++ proyectotimmy/src/views/Home.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido  al curso de Cálculo Integral  !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#33FF66',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
