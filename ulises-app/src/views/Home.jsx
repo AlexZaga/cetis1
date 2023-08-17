5f8b53bc915a80d2d9a35e59422f2ed065a15c6b
Ulises <ulisesjesus.lopez.ce1@dgeti.sep.gob.mx>
2023-08-17 12:03:45 -0600

    actualizando

--- ulises-app/src/views/Home.jsx
+++ ulises-app/src/views/Home.jsx
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D4C19C',
+    backgroundColor: '#9D2449',
     alignItems: 'center',
     justifyContent: 'center',
   },
