d940254707a9af9b5af52117f5a957220ddf6fc2
Syntia <yezita_lexus@hotmail.com>
2023-08-17 11:57:38 -0600

    syntia_parte1

--- syntia_react/App.js
+++ syntia_react/App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react' 
+import Main from './src/views/Main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>▬▬♦♠♠SANTOS♠♠♦▬▬</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <Main />
+  )
+  }
