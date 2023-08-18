7468aa1dc8ea53e64de888c054809a25137aa72f
Hugo <alexzaga@live.com.mx>
2023-08-18 13:39:25 -0600

    proyecto final v2

--- ciencia/src/views/Main.jsx
+++ ciencia/src/views/Main.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer} from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import Home from "./Home"
 import About from "./Acerca"
-
+import AreaTrabajo from "./areatrabajo"
 const Stack = createNativeStackNavigator()
 
 export default function Main(){
@@ -12,6 +12,7 @@ export default function Main(){
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="home" componet={Home} />
                 <Stack.Screen name="About" componet={About} />
+                <Stack.Screen name="Area de trabajo" componet={AreaTrabajo} />
             </Stack.Navigator>
         </NavigationContainer>
     )
