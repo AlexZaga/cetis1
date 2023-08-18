2d82224a1a49cda830f3cb7df15dd67fdeef3953
lazaro <cetibots@gmail.com>
2023-08-18 12:37:21 -0600

    Cubano 2

--- cubano_react/src/views/Main.jsx
+++ cubano_react/src/views/Main.jsx
@@ -3,15 +3,17 @@ import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStacknavigator} from '@react-navigation/native-stack'
 import Home from './Home'
 import About from ' ./About'
+import AreadeTrabajo from './AreadeTrabajo'
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStacknavigator()
 
 export default function Main(){
 return(
 <NavigationContainer>
 <Stack.Navigator initialRouteName="Home">
 <Stack.Screen Name='Home' component={Home} />  
-<Stack.Screen Name='Home' component={About} />   
+<Stack.Screen Name='About' component={About} />  
+<Stack.Screen Name='AreadeTrabajo' component={AreadeTrabajo} />  
 </Stack.Navigator>
 </NavigationContainer> 
 )
