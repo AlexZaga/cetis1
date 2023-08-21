d3f770bb79d240b77db8796016254e219508b712
Syntia <yezita_lexus@hotmail.com>
2023-08-21 13:49:54 -0600

    syntiafinal

--- syntia_react/src/views/AreaTrabajo.jsx
+++ syntia_react/src/views/AreaTrabajo.jsx
@@ -5,9 +5,9 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 export default function About({ navigation }) {
     return (
         <View style={styles.container}>
-            <Image source={require('../images/imagen1.png')} style={styles.image} />
-            <Text style={styles.text}>Esta es una aplicaci&oacute;n Demo creada para el curso de React Native en el plantel del CETIS No. 1.</Text>
-            <Text style={styles.text}>“Los sabios son los que buscan la sabiduría; los necios piensan ya haberla encontrado”. Napoleón Bonaparte.</Text>            
+            <Image source={require('../images/imagen2.png')} style={styles.image} />
+            <Text style={styles.text}>La cultura se adquiere leyendo libros; pero el conocimiento del mundo, que es mucho más necesario, sólo se alcanza leyendo a los hombres y estudiando las diversas ediciones que de ellos existen”. Lord Chesterfield.</Text>
+            <Text style={styles.text}>La amistad duplica las alegrías y divide las angustias por la mitad</Text>            
             <StatusBar style="auto" hidden={true} />
             <View>
                 <Button title='Volver al inicio' onPress={() => navigation.navigate('Home')} />
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#fff',
+        backgroundColor: '#8FF7E6',
         alignItems: 'center',
     },
     image: {
--- syntia_react/src/views/Home.jsx
+++ syntia_react/src/views/Home.jsx
@@ -1,20 +1,76 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Button, Platform } from 'react-native';
 
-export default function App() {
+export default function Splash({ navigation }) {
+
   return (
-    <View style={styles.container}>
-      <Text>▬▬♦♠♠♠♠♦▬▬</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <ImageBackground source={null} resizeMode='cover' style={styles.image}>
+          <Image style={styles.tinylogo} source={{ uri: 'https://matematicas.uno/wp-content/uploads/2020/11/Geometria-analitica-1.png' }} />
+          <Text style={styles.mensaje}></Text>
+         <Text style={styles.mensaje}>Centro de Estudios Tecnol&oacute;gicos</Text>
+          <Text style={styles.mensaje}>Industriales y de Servicio</Text>
+          <Text style={styles.mensaje}>No. 1</Text>
+          <Text style={styles.mensaje}>Realizado por: Syntia Hernandez Estrada</Text>
+
+          <View style={styles.buttons_area}>
+            <Button title='Area de Trabajo' onPress={() => navigation.navigate('Area de Trabajo')} />
+            <Text>&nbsp;&nbsp;&nbsp;&nbsp;</Text>
+            <Button title='Acerca de...' onPress={() => navigation.navigate('About')} />
+          </View>
+          
+          <View style={styles.footer}>
+            <Text style={styles.version}>Cetis no. 1</Text>
+          </View>
+        </ImageBackground>
+        <StatusBar style="auto" hidden={true} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  mensaje: {
+    textAlign: 'center',
+    color: '#000',
+    fontSize: 25
+  },
+  tinylogo: {
+    width: 350,
+    height: 380,
+  },
+  text : {
+    color: '#E704B7'
+  },
+  spinner: {
+    marginTop: 25
+  },
+  image: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 35
   },
-});
+  version : {
+    color: '#006',
+    fontSize: 12,
+    marginTop: 15
+  },
+  button: {
+    backgroundColor: '#009999',
+  },
+  footer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  buttons_area: {
+    flex: 1,
+    flexDirection: 'row'
+  }
+})
